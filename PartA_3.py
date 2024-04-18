197ef394d3de36492dbc907d16351024d00c7789
aak3m1 <202218854@zu.ac.ae>
2024-04-19 01:52:50 +0400

    PartA_3
    here we added comments

--- PartA_3.py
+++ PartA_3.py
@@ -2,6 +2,9 @@
 import heapq
 
 class Post:
+    """
+    this class represents the
+    """
     def __init__ (self, message, views):
         self.message = message #initializing the message of the post
         self.views = views #initializing the number of views the post has
@@ -9,25 +12,41 @@ class Post:
     def __lt__(self, other):
         #defining the lessthan operator as the opposite to implement
         #max heap based on views
-        return self.views > other.views
-    def __repr__(self):
-        return f"Post(message='{self.message}', views={self.views})"
+        return self.views > other.views  #reversing the comparison to create a max-heap based on views
 
+    def __repr__(self):
+        #providing a formal string representation of the Post object
+        #helps in debugging
+        return f"Post(message='{self.message}', views={self.views})"
 class MaxHeap:
+    """
+    this class represents
+    """
     def __init__(self):
-        self.heap = []
+        self.heap = [] #creating an empty list to store the heap elements
 
     def push (self, post):
+        #pushing a new post into the heap. 'heapq.heappush' is used, which adds the element
+        #reorders the heap to maintain the heap property
         heapq.heappush(self.heap,post)
     def pop(self):
+        #removing and return the largest element from the heap (due to max-heap property)
+        #if the heap is empty, returns None
         return heapq.heappop(self.heap) if self.heap else None
     def peek(self):
+        #returning the largest element without removing it from the heap
+        #returing None if the heap is empty
         return self.heap[0] if self.heap else None
 
+#creating an instance of MaxHeap
 max = MaxHeap()
-max.push(Post("Cats and Dogs!", 100))
-max.push(Post("Say Hi to my new puppy:)", 200))
-max.push(Post("New country is visited!", 300))
 
+#Pushing posts with different view counts into the heap
+max.push(Post("Cats and Dogs!", 1091))
+max.push(Post("Say Hi to my new puppy:)", 2303))
+max.push(Post("New country is visited!", 3209))
+
+#Pop the post with the most views from the heap and print it
 print(max.pop())
+#Peek at the next post with the most views without removing it and print it
 print(max.peek())
